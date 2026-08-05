40d1defaa4c9fbc3ad032c4c104e2c2e2b40e698
Luis Javier SAO <luisjaviersao@gmail.com>
2026-08-05 12:54:13 -0600

    Create sw.js
    "agrego service worker"

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'margarita-pressure-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
